9f218a0191d4f9b2c3eb454f25180a942bc456ad
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2018-11-15 23:31:19 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+
 var express = require('express');
 
 var app = express();
 
 
 var server = app.listen(process.env.PORT || 3000, listen);
+
 
 function listen() {
   var host = server.address().address;
@@ -14,19 +16,25 @@ function listen() {
 app.use(express.static('public'));
 
 
-var io = require('socket.io')(server);
 
+var io = require('socket.io')(server);
+var painti = []
+var indexi = 0
 
 io.sockets.on('connection',
+
+
+
+
   function (socket) {
   
     console.log("We have a new client: " + socket.id);
   
     socket.on('mouse',
       function(data) {
-
+      indexi = indexi + 1
+      painti[indexi] = data
         socket.broadcast.emit('mouse', data);
-        
 
       }
     );
@@ -35,16 +43,27 @@ io.sockets.on('connection',
       function(data) {
 
         console.log("reset");
-      
-
+        painti = []
+        indexi = 0
         socket.broadcast.emit('reset', data);
-        
+
 
       }
     );
+
+  socket.on('inforeq',
+      function(data) {
+        for (var i = 1; i < painti.length; i++) {
+    io.emit('mouse', painti[i]);
+  }
+
+      }
+    );
+      
     
     socket.on('disconnect', function() {
       console.log("Client has disconnected");
     });
   }
 );
+
