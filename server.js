e0d3774c453f017a15e6820d4e14a9b9b4dbbebb
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2018-11-17 23:06:52 -0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var server = app.listen(process.env.PORT || 3000, listen);
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+  console.log('Server Listening at http://' + host + ':' + port);
 }
 
 app.use(express.static('public'));
